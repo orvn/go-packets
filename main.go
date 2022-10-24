d5ab08dd94b9c7f046b14829eea21938690e577f
orun <orun@agencyundone.com>
2022-10-24 03:44:11 +0100

    feat: concurrent port scanning

--- main.go
+++ main.go
@@ -3,18 +3,57 @@ package main
 import (
 	"fmt"
 	"net"
+	"sort"
 )
 
-func main() {
-	for i := 1; i <= 1024; i++ {
-		// todo: select ports or port range
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+func worker(ports, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
 			// either the port is firewalled/filtered or closed
+			results <- 0
 			continue
 		}
 		conn.Close()
-		fmt.Printf("Local port %d is open\n", i)
+		results <- p
+	}
+}
+
+func main() {
+	// this channel will receive ports to be scanned
+	// invoke 32 workers
+	ports := make(chan int, 32)
+	// this channel will receive results of scanning
+	results := make(chan int)
+	// create a slice to store the results so that they can be sorted later.
+	var openports []int
+
+	// create a pool of workers
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	// send ports to be scanned
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	// close the channels
+	close(ports)
+	close(results)
+	// sort open port numbers
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
 	}
 }
